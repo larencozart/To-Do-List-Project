87282803dcd05dfe6171656142f923b54d6e5ddf
larebear007 <larencozart@gmail.com>
2024-01-30 21:59:18 -0600

    Add ToDo class, Add ToDoList class

---
+++ todo_list.js
@@ -0,0 +1,103 @@
+// This class represents a todo item and its associated
+// data: the todo title and a flag that shows whether the
+// todo item is done.
+
+class Todo {
+  static DONE_MARKER = "X";
+  static UNDONE_MARKER = " ";
+
+  constructor(title) {
+    this.title = title;
+    this.done = false;
+  }
+
+  toString() {
+    let marker = this.isDone() ? Todo.DONE_MARKER : Todo.UNDONE_MARKER;
+    return `[${marker}] ${this.title}`;
+  }
+
+  markDone() {
+    this.done = true;
+  }
+
+  markUndone() {
+    this.done = false;
+  }
+
+  isDone() {
+    return this.done;
+  }
+
+  getTitle() {
+    return this.title;
+  }
+}
+
+class TodoList {
+  constructor(title) {
+    this.title = title;
+    this.todos = [];
+  }
+
+  toString() {
+    let title = `---- ${this.title} ----`;
+    let list = this.todos.map(todo => todo.toString()).join("\n");
+    return `${title}\n${list}`;
+  }
+
+  add(todoObj) {
+    if (!(todoObj instanceof Todo)) {
+      throw new TypeError("can only add Todo objects");
+    }
+
+    this.todos.push(todoObj);
+  }
+
+  size() {
+    return this.todos.length;
+  }
+
+  first() {
+    return this.todos[0];
+  }
+
+  last() {
+    return this.todos[this.todos.length - 1];
+  }
+
+  itemAt(index) {
+    this._validateIndex(index);
+    return this.todos[index];
+  }
+
+  _validateIndex(index) { // _ in name suggests a "private" method
+    if (!(index in this.todos)) {
+      throw new ReferenceError(`invalid index: ${index}`);
+    }
+  }
+}
+
+// Omitted code
+
+// The Todo class
+// Your TodoList class
+
+let list = new TodoList("Today's Todos");
+
+
+let todo1 = new Todo("Buy milk");
+let todo2 = new Todo("Clean room");
+let todo3 = new Todo("Go to the gym");
+let todo4 = new Todo("Go shopping");
+
+list.add(todo1);
+list.add(todo2);
+list.add(todo3);
+list.add(todo4);
+// console.log(list);
+
+
+console.log(list.itemAt(1));
+// Omitted code
+
+console.log(list.itemAt(55)); // delete this line after testing it
