a550a313486b50265a3f1f05e6188b478e6ff143
larebear007 <larencozart@gmail.com>
2024-01-31 20:10:38 -0600

    Add additional required methods to TodoList class

--- todo_list.js
+++ todo_list.js
@@ -45,6 +45,21 @@ class TodoList {
     return `${title}\n${list}`;
   }
 
+  forEach(callback, thisArg) {
+    this.todos.forEach(callback, thisArg);
+  }
+
+  filter(callback, thisArg) {
+    let newList = new TodoList(this.title);
+    this.forEach(todo => {
+      if (callback(todo)) {
+        newList.add(todo);
+      }
+    });
+
+    return newList;
+  }
+
   add(todoObj) {
     if (!(todoObj instanceof Todo)) {
       throw new TypeError("can only add Todo objects");
@@ -62,7 +77,7 @@ class TodoList {
   }
 
   last() {
-    return this.todos[this.todos.length - 1];
+    return this.todos[this.size() - 1];
   }
 
   itemAt(index) {
@@ -75,29 +90,60 @@ class TodoList {
       throw new ReferenceError(`invalid index: ${index}`);
     }
   }
+
+  markDoneAt(index) {
+    this.itemAt(index).markDone();
+  }
+
+  markUndoneAt(index) {
+    this.itemAt(index).markUndone();
+  }
+
+  isDone() {
+    return this.todos.every(todo => todo.isDone());
+  }
+
+  shift() {
+    return this.todos.shift();
+  }
+
+  pop() {
+    return this.todos.pop();
+  }
+
+  removeAt(index) {
+    this._validateIndex(index);
+    return this.todos.splice(index, 1)[0];
+  }
+
+  findByTitle(title) {
+    return this.filter(todo => todo.getTitle() === title).first();
+  }
+
+  allDone() {
+    return this.filter(todo => todo.isDone());
+  }
+
+  allNotDone() {
+    return this.filter(todo => !todo.isDone());
+  }
+
+  markDone(title) {
+    let foundTodo = this.findByTitle(title);
+    if (foundTodo) {
+      foundTodo.markDone();
+    }
+  }
+
+  markAllDone() {
+    this.forEach(todo => todo.markDone());
+  }
+
+  markAllUndone() {
+    this.forEach(todo => todo.markUndone());
+  }
+
+  toArray() {
+    return this.todos.slice();
+  }
 }
-
-// Omitted code
-
-// The Todo class
-// Your TodoList class
-
-let list = new TodoList("Today's Todos");
-
-
-let todo1 = new Todo("Buy milk");
-let todo2 = new Todo("Clean room");
-let todo3 = new Todo("Go to the gym");
-let todo4 = new Todo("Go shopping");
-
-list.add(todo1);
-list.add(todo2);
-list.add(todo3);
-list.add(todo4);
-// console.log(list);
-
-
-console.log(list.itemAt(1));
-// Omitted code
-
-console.log(list.itemAt(55)); // delete this line after testing it
